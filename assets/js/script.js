c947fa437e4513e40eb7530a27922b7d3fb40186
DevMatheusGama <devmatheusgama@gmail.com>
2025-06-26 18:29:41 -0300

    finalisando o card, html, javascript e css feito

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+function mudarCorCoracao() {
+    let coracaoOrange = document.querySelector('.inputcoracao')
+    coracaoOrange.classList.toggle('cor-laranja')
+}
+
+function mudarCorCoracao(botao) {
+    botao.classList.toggle('cor-laranja');
+}
